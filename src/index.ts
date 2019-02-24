45eb896fee25a58ce445d08ea4d07a03242dd900
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-02-24 14:38:49 +0800

    Move all server code to server.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
+import { load } from "dotenv";
+load();
 
-const PORT = process.env.PORT || 5000;
-const app = express();
-
-function listenPort() {
-  console.log(`Listening for request on port ${PORT}`);
-}
-
-app.listen(PORT, listenPort);
+export const app = express();
 
 export const fn = (a: number, b: number) => a + b;
