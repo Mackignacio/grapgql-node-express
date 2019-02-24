ed351d7c84ad84073d2b75b9965efd2633b32411
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-02-24 13:33:47 +0800

    Setup a simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+function listenPort() {
+  console.log(`Listening for request on port ${PORT}`);
+}
+
+app.listen(PORT, listenPort);
+
 export const fn = (a: number, b: number) => a + b;
