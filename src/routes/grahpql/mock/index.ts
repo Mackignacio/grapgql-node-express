2a9e2b1974963a11d47b558007fae963dda3393b
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-02-28 02:03:40 +0800

    Update users

--- src/routes/grahpql/mock/index.ts
+++ src/routes/grahpql/mock/index.ts
@@ -2,7 +2,7 @@ const users = [
   { name: "Mack Ignacio", account_type: "admin", id: "1", product_id: "1" },
   { name: "Ralph Largo", account_type: "user", id: "2", product_id: "2" },
   { name: "Nemuel Lim", account_type: "user", id: "3", product_id: "2" },
-  { name: "Bill Gates", account_type: "user", id: "4", product_id: "1" },
+  { name: "Bill Gates", account_type: "user", id: "4", product_id: "3" },
   { name: "Steve Jobs", account_type: "admin", id: "5", product_id: "3" },
   { name: "Allan turing", account_type: "admin", id: "6", product_id: "3" },
 ];
