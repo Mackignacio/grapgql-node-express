53c00cec55f9480204fd0c3c69249cdd899f8cdf
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-02-27 22:06:40 +0800

    Change imports to use GraphQLType only

--- src/routes/grahpql/schema/index.ts
+++ src/routes/grahpql/schema/index.ts
@@ -1,19 +1,11 @@
-import { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } from "graphql";
-import { findBook } from "./lib/UserType";
-import { UserType } from "./lib/GrahpQLType";
+import { GraphQLObjectType, GraphQLSchema } from "graphql";
+import { user, product } from "./lib/GrahpQLType";
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    user: {
-      type: UserType,
-      args: {
-        id: { type: GraphQLID },
-      },
-      resolve(parents, args) {
-        return findBook(args.id);
-      },
-    },
+    user,
+    product,
   },
 });
 
