70a216181fc6f1691be61a72c64c69f69f0827f4
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-02-27 21:33:34 +0800

    Add graphql id

--- src/routes/grahpql/schema/lib/GrahpQLType.ts
+++ src/routes/grahpql/schema/lib/GrahpQLType.ts
@@ -1,9 +1,9 @@
-import { GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLObjectType, GraphQLString, GraphQLID } from "graphql";
 
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     account_type: { type: GraphQLString },
   }),
--- src/routes/grahpql/schema/lib/UserType.ts
+++ src/routes/grahpql/schema/lib/UserType.ts
@@ -1,8 +1,8 @@
 import { users } from "../../mock";
 import _ from "lodash";
 
-const findBooks = (id: any) => {
+const findBook = (id: any) => {
   return _.find(users, { id });
 };
 
-export { findBooks };
+export { findBook };
