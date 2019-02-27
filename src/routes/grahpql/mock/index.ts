78aa50fd49f05a9a23b2f180db698c79b053b6ab
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-02-28 01:41:28 +0800

    Add product_id

--- src/routes/grahpql/mock/index.ts
+++ src/routes/grahpql/mock/index.ts
@@ -1,7 +1,7 @@
 const users = [
-  { name: "Mack Ignacio", account_type: "admin", id: "1" },
-  { name: "Ralph Largo", account_type: "user", id: "2" },
-  { name: "Nemuel Lim", account_type: "user", id: "3" },
+  { name: "Mack Ignacio", account_type: "admin", id: "1", product_id: "1" },
+  { name: "Ralph Largo", account_type: "user", id: "2", product_id: "1" },
+  { name: "Nemuel Lim", account_type: "user", id: "3", product_id: "2" },
 ];
 
 const products = [{ name: "Websites", price: 100, id: "1" }, { name: "Mobile App", price: 250, id: "2" }, { name: "Desktop App", price: 150, id: "3" }];
