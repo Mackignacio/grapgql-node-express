ce017413711d709396cf844ebf11146399c060e2
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-02-27 22:06:07 +0800

    Make GraphQLType as the main export file for each types

--- src/routes/grahpql/schema/lib/GrahpQLType.ts
+++ src/routes/grahpql/schema/lib/GrahpQLType.ts
@@ -1,12 +1,2 @@
-import { GraphQLObjectType, GraphQLString, GraphQLID } from "graphql";
-
-const UserType = new GraphQLObjectType({
-  name: "User",
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    account_type: { type: GraphQLString },
-  }),
-});
-
-export { UserType };
+export * from "./ProductType";
+export * from "./UserType";
