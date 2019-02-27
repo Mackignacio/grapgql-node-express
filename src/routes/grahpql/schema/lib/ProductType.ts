c2a93cca2a76d316a77095ba293b1ed83a66ff89
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-02-27 22:07:12 +0800

    Add ProductType.ts

---
+++ src/routes/grahpql/schema/lib/ProductType.ts
@@ -0,0 +1,28 @@
+import { products } from "../../mock";
+import { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt } from "graphql";
+import _ from "lodash";
+
+const findProduct = (id: any) => {
+  return _.find(products, { id });
+};
+
+const ProductType = new GraphQLObjectType({
+  name: "Product",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    price: { type: GraphQLInt },
+  }),
+});
+
+const product = {
+  type: ProductType,
+  args: {
+    id: { type: GraphQLID },
+  },
+  resolve(parents: any, args: any) {
+    return findProduct(args.id);
+  },
+};
+
+export { product, findProduct };
