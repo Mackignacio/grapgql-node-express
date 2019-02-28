1ef8cf18b1822c59d93229e88f3668249b22381d
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-02-28 14:23:54 +0800

    Add models for mongoose

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import { users } from "./lib/user.model";
+import { products } from "./lib/product.model";
+
+const models: { [k: string]: any } = {
+  users,
+  products,
+};
+
+export default models;
